1dc88b544879de8e274748ed2b1154286f61e665
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-05-04 02:00:36 -0700

    add a simple UI for rock-paper-scissors game

--- javascript-basics/rock-paper-scissors.js
+++ javascript-basics/rock-paper-scissors.js
@@ -1,4 +1,4 @@
-// see https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+// see https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -17,43 +17,61 @@ function getComputerChoice() {
     return choice;
 }
 
+let computerScore = 0;
+let playerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
-    const playerSelectionLower = playerSelection.toLowerCase();
-    const computerSelectionLower = computerSelection.toLowerCase();
-    let result;
-    if (computerSelectionLower === playerSelectionLower) {
-        result = "Draw! (" + computerSelection + ")";
-    } else if (computerSelectionLower === 'rock' && playerSelectionLower == 'scissors'
-        || computerSelectionLower === 'paper' && playerSelectionLower == 'rock'
-        || computerSelectionLower === 'scissors' && playerSelectionLower == 'paper') {
-        result = "You lose! " + computerSelection + " beats " + playerSelection;
-    } else {
-        result = "You win! " + playerSelection + " beats " + computerSelection;
+
+    if (computerSelection === playerSelection) {
+        return {
+            code: "draw",
+            message: "Draw! (" + computerSelection + ")"
+        };
     }
-    return result;
+
+    if (computerSelection === 'Rock' && playerSelection == 'Scissors'
+        || computerSelection === 'Paper' && playerSelection == 'Rock'
+        || computerSelection === 'Scissors' && playerSelection == 'Paper') {
+        computerScore++;
+        return {
+            code: "loss",
+            message: "You lose! " + computerSelection + " beats " + playerSelection
+        };
+    }
+
+    playerScore++;
+    return {
+        code: "win",
+        message: "You win! " + playerSelection + " beats " + computerSelection
+    };
 }
 
-function isValidSelection(selection) {
-    const selectionLower = (selection ?? "").toLowerCase();
-    return selectionLower === 'rock' || selectionLower === 'paper' || selectionLower === 'scissors';
-}
+let resultDiv = document.querySelector('div[id=result]');
+let resultMsgDiv = document.createElement('div');
+resultDiv.appendChild(resultMsgDiv);
+resultDiv.appendChild(document.createElement('br'));
 
 function game() {
-    let round = 1;
-    while (round <= 5) {
-        let playerSelection = prompt("Round " + round + ": Rock, Paper, or Scissors?");
+    if (playerScore < 5 && computerScore < 5) {
+        const result = playRound(this.id, getComputerChoice());
 
-        if (playerSelection === null) {
-            console.log("Canceled");
-            break;
-        }
-        if (isValidSelection(playerSelection)) {
-            let computerSelection = getComputerChoice();
-            console.log("Round " + round + ": " + playRound(playerSelection, computerSelection));
-            round++;
+        resultMsgDiv.textContent = "SCORE: you: " + playerScore + ", computer: " + computerScore;
+
+        let div = document.createElement('div');
+        div.textContent = result.message;
+        div.classList.add(result.code);
+        resultDiv.appendChild(div);
+
+        if (playerScore === 5 || computerScore === 5) {
+            resultMsgDiv.textContent = "GAME OVER: "
+                + (playerScore > computerScore ? "you" : "computer") + " won.";
         }
     }
-
 }
 
-game();
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', game);
+});
+
