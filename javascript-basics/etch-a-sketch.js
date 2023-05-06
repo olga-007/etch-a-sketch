fbcb8f7f5ec96fc8eaa5b0c327ae70e7d6e58ba0
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-05-05 17:33:02 -0700

    Draw etch-a-sketch grid using flexbox

--- javascript-basics/etch-a-sketch.js
+++ javascript-basics/etch-a-sketch.js
@@ -1,1 +1,19 @@
 // see https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+function drawGrid(gridSize) {
+    const container = document.getElementById('container');
+
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+    }
+}
+
+drawGrid(10);
